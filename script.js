d57f897dd551f48519708932258a223ede477cba
Amanda LeMoine <amanda1@mnlemoines.com>
2020-10-26 20:12:52 -0700

    html file updated with time blocks, and js updated with comparing time values

--- script.js
+++ script.js
@@ -38,3 +38,31 @@ console.log(moment().format('dddd, MMMM Do'));
 // display current date within #currendDay p
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
+function compareTime(time){
+    // compare the current time to an hour time block
+
+    // for loop to iterate through my "timeblock" divs for each one check:
+
+    // if the current time is before the time block update th div class to .past
+    // if the current time is within the time block update the div class to .present
+    // if the current time is after the time block update the div class to .future
+}
+
+var format = 'hh:mm:ss'
+
+// var time = moment() gives you current time. no format required.
+var time = moment();
+  beforeTime = moment('20:00:00', format);
+  afterTime = moment('20:59:59', format);
+  console.log(time+" "+beforeTime+" "+afterTime);
+
+
+if (time.isBetween(beforeTime, afterTime)) {
+
+  console.log('is between')
+
+} else {
+
+  console.log('is not between')
+
+}
